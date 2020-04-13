4d99b4d1133af26638f691d176cb6425d74bda30
Gustaf Lindholm <babiluskus@gmail.com>
2020-04-13 18:42:12 +0200

    setMain route for photo, get main photo function for repository

--- Controllers/PhotosController.cs
+++ Controllers/PhotosController.cs
@@ -51,7 +51,7 @@ namespace DatingApp.API.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> AddPhotoForUser(int userid, 
+        public async Task<IActionResult> AddPhotoForUser(int userid,
         [FromForm]PhotoForCreationDto photoForCreationDto)
         {
             if (userid != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
@@ -99,6 +99,33 @@ namespace DatingApp.API.Controllers
             }
 
             return BadRequest("Could not save photo");
+        }
+
+        [HttpPost("{id}/setMain")]
+        public async Task<IActionResult> SetMainPhoto(int userId, int id)
+        {
+            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
+
+            var user = await _repo.GetUser(userId);
+
+            if (!user.Photos.Any(p => p.id == id))
+                return Unauthorized();
+
+            var photoFromRepo = await _repo.GetPhoto(id);
+
+            if (photoFromRepo.IsMain)
+                return BadRequest("This is already the main photo");
+
+            var currentMainPhoto = await _repo.GetMainPhotoForUser(userId);
+            currentMainPhoto.IsMain = false;
+
+            photoFromRepo.IsMain = true;
+
+            if (await _repo.SaveAll())
+                return NoContent();
+
+            return BadRequest("Could not set photo to main");
         }
     }
 }
--- Data/IDatingRepository.cs
+++ Data/IDatingRepository.cs
@@ -9,11 +9,13 @@ namespace DatingApp.API.Data
         // "generic typeOf method". Add T (User|Photo) takes entity as parameter and
         // is constrained to just classes. T is typeOf class.
         // same method for both User and Photo
-         void Add<T>(T entity) where T: class;
-         void Delete<T>(T entity) where T: class;
-         Task<bool> SaveAll();
-         Task<IEnumerable<User>> GetUsers();
-         Task<User> GetUser(int id);
-         Task<Photo> GetPhoto(int id);
+        void Add<T>(T entity) where T : class;
+        void Delete<T>(T entity) where T : class;
+        Task<bool> SaveAll();
+        Task<IEnumerable<User>> GetUsers();
+        Task<User> GetUser(int id);
+        Task<Photo> GetPhoto(int id);
+
+        Task<Photo> GetMainPhotoForUser(int userId);
     }
 }
