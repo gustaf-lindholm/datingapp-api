cec46de0e7c427cef2cde8e78908c9765113b00f
Gustaf Lindholm <babiluskus@gmail.com>
2020-06-08 15:54:17 +0200

    migration, interface and repository methods for messages

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -17,6 +17,8 @@ namespace DatingApp.API.Data
 
     public DbSet<Like> Likes { get; set; }
 
+    public DbSet<Message> Messages { get; set; }
+
     // override default db table creation
     protected override void OnModelCreating(ModelBuilder builder)
     {
@@ -36,6 +38,17 @@ namespace DatingApp.API.Data
           .WithMany(u => u.Likees)
           .HasForeignKey(u => u.LikerId)
           .OnDelete(DeleteBehavior.Restrict);
+
+
+      builder.Entity<Message>()
+        .HasOne(u => u.Sender)
+        .WithMany(m => m.MessagesSent)
+        .OnDelete(DeleteBehavior.Restrict);
+
+      builder.Entity<Message>()
+        .HasOne(u => u.Recipient)
+        .WithMany(m => m.MessagesReceived)
+        .OnDelete(DeleteBehavior.Restrict);
     }
 
   }
