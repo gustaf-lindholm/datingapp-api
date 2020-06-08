51773145b9de8925a11726da1006f3bb37c2e1da
Gustaf Lindholm <babiluskus@gmail.com>
2020-06-08 12:45:42 +0200

    like user controller method, with properties in repository and userparams

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -7,72 +7,107 @@ using AutoMapper;
 using DatingApp.API.Data;
 using DatingApp.API.Dtos;
 using DatingApp.API.Helpers;
+using DatingApp.API.models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DatingApp.API.Controllers
 {
-    [ServiceFilter(typeof(LogUserActivity))]
-    [Authorize]
-    [Route("api/[controller]")]
-    [ApiController]
-    public class UsersController : ControllerBase
+  [ServiceFilter(typeof(LogUserActivity))]
+  [Authorize]
+  [Route("api/[controller]")]
+  [ApiController]
+  public class UsersController : ControllerBase
+  {
+    private readonly IDatingRepository _repo;
+    private readonly IMapper _mapper;
+    public UsersController(IDatingRepository repo, IMapper mapper)
     {
-        private readonly IDatingRepository _repo;
-        private readonly IMapper _mapper;
-        public UsersController(IDatingRepository repo, IMapper mapper)
-        {
-            _mapper = mapper;
-            _repo = repo;
-        }
-
-        [HttpGet]
-        public async Task<IActionResult> GetUsers([FromQuery]UserParams userParams)
-        {
-            var currentUserId = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value);
-
-            var userFromRepo = await _repo.GetUser(currentUserId);
-
-            userParams.UserId = currentUserId;
-            
-
-            if (string.IsNullOrEmpty(userParams.Gender))
-            {
-                userParams.Gender = userFromRepo.Gender == "male" ? "female" : "male";
-            }
-
-            var users = await _repo.GetUsers(userParams);
-            var usersToReturn = _mapper.Map<IEnumerable<UserForListDto>>(users);
-
-            Response.AddPagination(users.CurrentPage, users.PageSize, users.TotalCount, users.TotalPages);
-
-            return Ok(usersToReturn);
-        }
-
-        [HttpGet("{id}", Name = "GetUser")]
-        public async Task<IActionResult> GetUser(int id)
-        {
-            var user = await _repo.GetUser(id);
-
-            var userToReturn = _mapper.Map<UserForDetailedDto>(user);
-
-            return Ok(userToReturn);
-        }
-
-        [HttpPut("{id}")]
-        public async Task<IActionResult> UpdateUser(int id, UserForUpdateDto userForUpdateDto)
-        {
-            if(id != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
-                return Unauthorized();
-
-            var userFromRepo = await _repo.GetUser(id);
-
-            _mapper.Map(userForUpdateDto, userFromRepo);
-
-            if(await _repo.SaveAll())
-                return NoContent();
-
-            throw new Exception($"Updating user {id} failed on save");
-        }
+      _mapper = mapper;
+      _repo = repo;
     }
+
+    [HttpGet]
+    public async Task<IActionResult> GetUsers([FromQuery] UserParams userParams)
+    {
+      var currentUserId = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value);
+
+      var userFromRepo = await _repo.GetUser(currentUserId);
+
+      userParams.UserId = currentUserId;
+
+
+      if (string.IsNullOrEmpty(userParams.Gender))
+      {
+        userParams.Gender = userFromRepo.Gender == "male" ? "female" : "male";
+      }
+
+      var users = await _repo.GetUsers(userParams);
+      var usersToReturn = _mapper.Map<IEnumerable<UserForListDto>>(users);
+
+      Response.AddPagination(users.CurrentPage, users.PageSize, users.TotalCount, users.TotalPages);
+
+      return Ok(usersToReturn);
+    }
+
+    [HttpGet("{id}", Name = "GetUser")]
+    public async Task<IActionResult> GetUser(int id)
+    {
+      var user = await _repo.GetUser(id);
+
+      var userToReturn = _mapper.Map<UserForDetailedDto>(user);
+
+      return Ok(userToReturn);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateUser(int id, UserForUpdateDto userForUpdateDto)
+    {
+      if (id != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+        return Unauthorized();
+
+      var userFromRepo = await _repo.GetUser(id);
+
+      _mapper.Map(userForUpdateDto, userFromRepo);
+
+      if (await _repo.SaveAll())
+        return NoContent();
+
+      throw new Exception($"Updating user {id} failed on save");
+    }
+
+
+    [HttpPost("{id}/like/{recipientId}")]
+    public async Task<IActionResult> LikeUser(int id, int recipientId)
+    {
+      // authorize user
+      if (id != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+        return Unauthorized();
+
+      // check if there already is a like
+      var like = await _repo.GetLike(id, recipientId);
+
+      if (like != null)
+        return BadRequest("You already liked this user");
+
+      // check if the user being liked exists
+      if (await _repo.GetUser(recipientId) == null)
+        return NotFound();
+
+      // create a new like
+      like = new Like
+      {
+        LikerId = id,
+        LikeeId = recipientId
+      };
+
+      // add to repo with a Like entity
+      _repo.Add<Like>(like);
+
+      if (await _repo.SaveAll())
+        return Ok();
+
+      return BadRequest("Failed to like user");
+    }
+  }
 }
--- Data/DatingRepository.cs
+++ Data/DatingRepository.cs
@@ -25,6 +25,11 @@ namespace DatingApp.API.Data
       _context.Remove(entity);
     }
 
+    public async Task<Like> GetLike(int userId, int recipientId)
+    {
+      return await _context.Likes.FirstOrDefaultAsync(u => u.LikerId == userId && u.LikeeId == recipientId);
+    }
+
     public async Task<Photo> GetMainPhotoForUser(int userId)
     {
       return await _context.Photos.Where(u => u.UserId == userId).FirstOrDefaultAsync(p => p.IsMain);
@@ -48,36 +53,66 @@ namespace DatingApp.API.Data
     {
       // get users from context without executing it at this time
       var users = _context.Users.Include(p => p.Photos).OrderByDescending(u => u.LastActive).AsQueryable();
-    
+
       users = users.Where(u => u.Id != userParams.UserId);
       users = users.Where(u => u.Gender == userParams.Gender);
+
+      if (userParams.Likers)
+      {
+        var userLikers = await GetUserLikes(userParams.UserId, userParams.Likers);
+        // check if id's from userlikes matches any users in users table
+        users = users.Where(u => userLikers.Contains(u.Id));
+      }
+
+      if (userParams.Likees)
+      { 
+        var userLikees = await GetUserLikes(userParams.UserId, userParams.Likers);
+        // check if id's from userlikes matches any users in users table
+        users = users.Where(u => userLikees.Contains(u.Id));
+      }
 
       // know that user specified age that they are looking for
       if (userParams.MinAge != 18 || userParams.MaxAge != 99)
       {
-          // calculate oldest birthday
-          var minDob = DateTime.Today.AddYears(-userParams.MaxAge - 1);
-          // calculate youngest birthday
-          var maxDob = DateTime.Today.AddYears(-userParams.MinAge);
+        // calculate oldest birthday
+        var minDob = DateTime.Today.AddYears(-userParams.MaxAge - 1);
+        // calculate youngest birthday
+        var maxDob = DateTime.Today.AddYears(-userParams.MinAge);
 
-          users = users.Where(u => u.DateOfBirth >= minDob && u.DateOfBirth <= maxDob);
+        users = users.Where(u => u.DateOfBirth >= minDob && u.DateOfBirth <= maxDob);
       }
 
-      if(!string.IsNullOrEmpty(userParams.OrderBy))
+      if (!string.IsNullOrEmpty(userParams.OrderBy))
       {
         switch (userParams.OrderBy)
         {
-            case "created":
-              users = users.OrderByDescending(u => u.Created);
-              break;
-            default:
-              users = users.OrderByDescending(u => u.LastActive);
-              break;
+          case "created":
+            users = users.OrderByDescending(u => u.Created);
+            break;
+          default:
+            users = users.OrderByDescending(u => u.LastActive);
+            break;
         }
       }
 
       // create and return new instance of PagedList
       return await PagedList<User>.CreateAsync(users, userParams.PageNumber, userParams.PageSize);
+    }
+
+    private async Task<IEnumerable<int>> GetUserLikes(int id, bool likers)
+    {
+      // get logged in user that includes likes and likees collection
+      var user = await _context.Users.Include(x => x.Likers).Include(x => x.Likees).FirstOrDefaultAsync(u => u.Id == id);
+
+      if (likers)
+      {
+        // return list of the likers of the currently logged in user
+        return user.Likers.Where(u => u.LikeeId == id).Select(i => i.LikerId);
+      }
+      else
+      {
+        return user.Likees.Where(u => u.LikerId == id).Select(i => i.LikeeId);
+      }
     }
 
     public async Task<bool> SaveAll()
