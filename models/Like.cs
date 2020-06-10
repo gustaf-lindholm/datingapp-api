5e0f83d6ad9994452cd06152742174709c22b6e4
Gustaf Lindholm <babiluskus@gmail.com>
2020-06-10 22:53:41 +0200

    configured lazy loading for include db queries

--- models/Like.cs
+++ models/Like.cs
@@ -1,15 +1,15 @@
 namespace DatingApp.API.models
 {
-    public class Like
-    {
-        // user ID's
-        public int LikerId { get; set; }
+  public class Like
+  {
+    // user ID's
+    public int LikerId { get; set; }
 
-        // user beeing liked 
-        public int LikeeId { get; set; }
-        
-        public User Liker { get; set; }
+    // user beeing liked 
+    public int LikeeId { get; set; }
 
-        public User Likee { get; set; }
-    }
+    public virtual User Liker { get; set; }
+
+    public virtual User Likee { get; set; }
+  }
 }
--- models/Message.cs
+++ models/Message.cs
@@ -2,28 +2,28 @@ using System;
 
 namespace DatingApp.API.models
 {
-    public class Message
-    {
-        public int Id { get; set; }
+  public class Message
+  {
+    public int Id { get; set; }
 
-        public int SenderId { get; set; }
+    public int SenderId { get; set; }
 
-        public User Sender { get; set; }
+    public virtual User Sender { get; set; }
 
-        public int RecipientId { get; set; }
+    public int RecipientId { get; set; }
 
-        public User Recipient { get; set; }
+    public virtual User Recipient { get; set; }
 
-        public string Content { get; set; }
+    public string Content { get; set; }
 
-        public bool IsRead { get; set; }
+    public bool IsRead { get; set; }
 
-        public DateTime? DateRead { get; set; }
+    public DateTime? DateRead { get; set; }
 
-        public DateTime MessageSent { get; set; }
+    public DateTime MessageSent { get; set; }
 
-        public bool SenderDeleted { get; set; }
+    public bool SenderDeleted { get; set; }
 
-        public bool RecipientDeleted { get; set; }
-    }
+    public bool RecipientDeleted { get; set; }
+  }
 }
